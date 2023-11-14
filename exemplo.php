e975a9dd5e952a5c39233ef0f52648610b191382
Daniel Baumann <143436969+baumanndaniel21@users.noreply.github.com>
2023-11-14 20:29:56 -0300

    teste

---
+++ exemplo.php
@@ -0,0 +1,10 @@
+<?php
+    echo "<h1>Hello World</h1>";
+    //Declaração de variavel
+    $nome = "Daniel Baumann";
+    echo "Boa noite $nome";
+    $num1 = 1;
+    $num2 = 3;
+    $soma = $num1 + $num2;
+    echo "<br>$soma";
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Folha de pagamento</title>
+</head>
+<body>
+    <form method="post">
+        <label for="valorHora">Quanto você ganha por hora?</label>
+        <input id="valorHora"type="password" placeholder="R$" name="valorHora">
+        <label for="horasTrabalhadas">Quantas horas trabalhadas no mês?</label>
+        <input id="horasTrabalhadas" type="text" placeholder="R$" name="horasTrabalhadas">
+        <input type="submit" value="Calcular">
+    </form>
+</body>
+</html>
